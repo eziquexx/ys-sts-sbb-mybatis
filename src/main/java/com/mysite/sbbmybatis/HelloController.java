b699739cd07e2164c50a8ef6ff8004cf3840af46
jelee <waftyann@gmail.com>
2024-10-07 11:43:24 +0900

    hello2.html 생성. hello world!

--- src/main/java/com/mysite/sbbmybatis/HelloController.java
+++ src/main/java/com/mysite/sbbmybatis/HelloController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class HelloController {
 	@GetMapping("/hello")
 	public String hello() {
-		return ("hello");
+		return ("hello2");
 	}
 	
 }
