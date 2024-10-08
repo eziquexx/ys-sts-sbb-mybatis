04503a01d6450057288dd6aecdc567c686f1826b
jelee <waftyann@gmail.com>
2024-10-08 16:53:48 +0900

    login 성공 작업 fin

--- src/main/java/com/mysite/sbbmybatis/UserController.java
+++ src/main/java/com/mysite/sbbmybatis/UserController.java
@@ -5,6 +5,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 
@@ -13,11 +15,21 @@ public class UserController {
 	@Autowired
 	private UserService userService;
 	
+	// 로그인 페이지
 	@GetMapping("/user/login")
 	public String login() {
 		return "/user/login";
 	}
 	
+	
+	@PostMapping("/user/login")
+//	@ResponseBody
+	public void login2(@RequestBody User user) {
+		User newUser = user;
+	}
+	
+	
+	// 상세보기 /usre/hong
 	@GetMapping("/user/{username}")
 	public String getMessage(@PathVariable("username") String username, Model model) {
 		User user = userService.getUser(username);
